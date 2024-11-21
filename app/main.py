beff66940e0740702c4a26f257eeb7684f6b5e68
Gumm11 <vaaa7673@gmail.com>
2024-11-21 16:15:30 +0700

    a

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from app.routes import clustering_router
+from fastapi.staticfiles import StaticFiles
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 # Initialize FastAPI app
 app = FastAPI()
 
 # Include the clustering router
 app.include_router(clustering_router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
